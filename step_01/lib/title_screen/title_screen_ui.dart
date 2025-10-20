8d20f143b60518725ad66a87a1f5deec26ff4256
Thando <njabulodhladla8@gmail.com>
2025-10-20 14:22:42 +0200

    New ui file added

---
+++ step_01/lib/title_screen/title_screen_ui.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:extra_alignments/extra_alignments.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../assets.dart';
+import '../common/ui_scaler.dart';
+import '../styles.dart';
+
+class TitleScreenUi extends StatelessWidget {
+  const TitleScreenUi({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 40, horizontal: 50),
+      child: Stack(
+        children: [
+          /// Title Text
+          TopLeft(
+            child: UiScaler(
+              alignment: Alignment.topLeft,
+              child: _TitleText(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
