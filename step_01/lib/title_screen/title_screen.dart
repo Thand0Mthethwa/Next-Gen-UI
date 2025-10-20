5358a57eb660f720d4e818b475387e0fe075ee39
Thando <njabulodhladla8@gmail.com>
2025-10-20 12:27:40 +0200

    Added blend colors

--- step_01/lib/title_screen/title_screen.dart
+++ step_01/lib/title_screen/title_screen.dart
@@ -47,5 +47,18 @@ class _LitImage extends StatelessWidget {
     required this.imgSrc,
     required this.lightAmt,
   });
-  
+
+  final Color color;
+  final String imgSrc;
+  final double lightAmt;
+
+  @override
+  Widget build(BuildContext context) {
+    final hsl = HSLColor.fromColor(color);
+    return Image.asset(
+      imgSrc,
+      color: hsl.withLightness(hsl.lightness * lightAmt).toColor(),
+      colorBlendMode: BlendMode.modulate,
+    );
+  }
 }
